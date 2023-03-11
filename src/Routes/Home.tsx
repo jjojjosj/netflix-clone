1ee722af2a7ed93dab06e3e50ee9a0626c2a2670
Sangjun Lee <jjojjosj@gmail.com>
2023-03-11 21:10:23 +0900

    모달창에서 영화정보 출력

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -90,12 +90,37 @@ const Overlay = styled(motion.div)`
 `;
 
 const BigMovie = styled(motion.div)`
-  position: "absolute";
-  width: "40vw";
-  height: "80vh";
+  position: absolute;
+  width: 40vw;
+  height: 80vh;
   left: 0;
   right: 0;
-  margin: "0 auto";
+  margin: 0 auto;
+  border-radius: 15px;
+  overflow: hidden;
+  background-color: ${(props) => props.theme.black.lighter};
+`;
+
+const BigCover = styled.div`
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  height: 400px;
+`;
+
+const BigTitle = styled.h3`
+  color: ${(props) => props.theme.white.lighter};
+  padding: 20px;
+  font-size: 46px;
+  position: relative;
+  top: -80px;
+`;
+
+const BigOverview = styled.p`
+  padding: 20px;
+  position: relative;
+  color: ${(props) => props.theme.white.lighter};
+  top: -80px;
 `;
 
 const rowVariants = {
@@ -164,6 +189,10 @@ function Home() {
   const onOverlayClick = () => {
     history.push(`/`);
   };
+  const clickedMovie =
+    bigMovieMatch?.params.movieId &&
+    data?.results.find((movie) => movie.id === +bigMovieMatch.params.movieId);
+
   return (
     <Wrapper>
       {isLoading ? (
@@ -222,7 +251,22 @@ function Home() {
                     top: scrollY.get() + 100,
                   }}
                   layoutId={bigMovieMatch.params.movieId}
-                />
+                >
+                  {clickedMovie && (
+                    <>
+                      <BigCover
+                        style={{
+                          backgroundImage: `linear-gradient(to top, black, transparent), url(${makeImagePath(
+                            clickedMovie.backdrop_path,
+                            "w500"
+                          )})`,
+                        }}
+                      />
+                      <BigTitle>{clickedMovie.title}</BigTitle>
+                      <BigOverview>{clickedMovie.overview}</BigOverview>
+                    </>
+                  )}
+                </BigMovie>
               </>
             ) : null}
           </AnimatePresence>
