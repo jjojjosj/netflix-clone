98f8c00cd386135a5f6a53eb6e7053ec8ab51f2a
Sangjun Lee <jjojjosj@gmail.com>
2023-03-12 12:31:26 +0900

    라우팅 변경

---
+++ src/Router.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+function Router() {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Header />
+      <Switch>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path={["/", "/movies/:movieId"]}>
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+export default Router;
